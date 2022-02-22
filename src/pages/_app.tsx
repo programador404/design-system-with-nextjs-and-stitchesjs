d62b4124c5288b4e311fc03a5bbb771ee3ed291f
Geziel Elyon <geziel_barbosa_@hotmail.com>
2022-02-21 21:29:54 -0300

    styles: add stitches theme and global styles on page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import { AppProps } from 'next/app';
 
+import { globalStyles } from '@/styles/global';
+
+globalStyles();
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,90 @@
+import { styled } from '@/styles';
+
+const Flex = styled(`div`, {
+  display: `flex`,
+  alignItems: `center`,
+});
+
+const Button = styled(`button`, {
+  border: 0,
+  appearance: `none`,
+  cursor: `pointer`,
+
+  transition: `background 0.2s`,
+  fontWeight: `bold`,
+
+  $$shadowColor: ``,
+
+  variants: {
+    color: {
+      primary: {
+        backgroundColor: `$primary`,
+        color: `#fff`,
+      },
+      shape: {
+        backgroundColor: `$shape`,
+        color: `#fff`,
+      },
+    },
+    size: {
+      small: {
+        padding: `$16 $32`,
+      },
+      full: {
+        padding: `$16 $32`,
+        flex: 1,
+      },
+    },
+    radius: {
+      default: {
+        borderRadius: `$default`,
+      },
+      full: {
+        borderRadius: `$full`,
+      },
+    },
+    shadow: {
+      true: {
+        boxShadow: `0px 0px 8px $$shadowColor`,
+      },
+    },
+  },
+
+  compoundVariants: [
+    {
+      color: `primary`,
+      shadow: true,
+      css: {
+        $$shadowColor: `$colors$primary`,
+      },
+    },
+    {
+      color: `shape`,
+      shadow: true,
+      css: {
+        $$shadowColor: `$colors$shape`,
+      },
+    },
+  ],
+
+  defaultVariants: {
+    color: `primary`,
+    size: `small`,
+    radius: `default`,
+    shadow: true,
+  },
+});
+
 export default function Home() {
-  return <h1>Hello Experts Club</h1>;
+  return (
+    <Flex css={{ gap: `$16`, padding: `$16` }}>
+      <h1>Hello Experts Club</h1>
+      <Button type="button" color="primary">
+        Experts Club
+      </Button>
+      <Button type="button" color="shape" size="full" radius="full">
+        Experts Club
+      </Button>
+    </Flex>
+  );
 }
